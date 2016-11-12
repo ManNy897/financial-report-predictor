274e2c84015883355674f234762d01a61bc085e1
Manny <mannya897@gmail.com>
2016-11-12 11:57:38 -0500

    Addded comments

--- src/FileExtractor.java
+++ src/FileExtractor.java
@@ -9,12 +9,18 @@ import java.time.format.DateTimeFormatter;
  * Created by macpro on 11/10/16.
  */
 public class FileExtractor {
+    //mapping file names to list containing date and company name of report
     HashMap<String, List<String>> fileInfo = new HashMap<String, List<String>>();
 
-    public FileExtractor(String dir){
+    public FileExtractor(String dir) {
         buildFilesMap(dir);
     }
 
+
+    /**
+     * Builds a map of the file names of the reports and the information we want (company name and date)
+     * @param dir Directory of the folder containing the metadata files
+     */
     public void buildFilesMap(String dir){
         File folder = new File(dir);
         File[] files = folder.listFiles((directory, name) -> !name.equals(".DS_Store"));
@@ -28,13 +34,18 @@ public class FileExtractor {
 
     }
 
+    /**
+     * Reads in each metadata file and adds the values to the map
+     * @param br
+     * @throws IOException
+     */
     public void addElementsFromFile(BufferedReader br) throws IOException {
         for(String line; (line = br.readLine()) != null;){
             String[] parts = line.split("\t");
             ArrayList<String> elements = new ArrayList<String>();
             String fileName = parts[0] + ".txt";
             String date = parts[1];
-            String companyName = parts[3].split(" /")[0];
+            String companyName = parts[3].split("/")[0].trim();
             elements.add(companyName);
             elements.add(date);
             fileInfo.put(fileName, elements);
@@ -42,21 +53,37 @@ public class FileExtractor {
         br.close();
     }
 
-    public HashMap<String, List<String>> getFileMap(){
-        return fileInfo;
-    }
+    /**
+     *
+     * @return the map containing file names company name and report date
+     */
+    //used just for testing
+//    public HashMap<String, List<String>> getFileMap(){
+//        return fileInfo;
+//    }
 
+    /**
+     * Returns the date of the report
+     * @param fileName
+     * @return localDate object representing the date of the report
+     */
     public LocalDate getReportDate(String fileName){
         String dateString = fileInfo.get(fileName).get(1);
         LocalDate  date = LocalDate.parse(dateString, DateTimeFormatter.BASIC_ISO_DATE);
         return date;
     }
 
+    /**
+     * Returns the company name of the specified report
+     * @param fileName
+     * @return
+     */
     public String getCompanyName(String fileName){
         String companyName = fileInfo.get(fileName).get(0);
         return companyName;
     }
 
+    //Not developed
  /*   public String getReportSymbol(String fileName){
 
     }
